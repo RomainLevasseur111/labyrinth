605e495b0b2e11e784db26cec80da53214783a09
Ignotus111 <romainlevasseur4@gmail.com>
2020-02-16 18:05:27 +0100

    verify if a map is a valid maze added directory with maps

--- classes/labyrinth.py
+++ classes/labyrinth.py
@@ -1,5 +1,32 @@
 class Labyrinth:
+    VALID_MAZE = True
+    def __init__(self, filename, maze_length=0):
+        self.filename = filename
+        self.maze_length = maze_length
 
+    @property
+    def is_maze_valid(self):
+        with open(self.filename, "r") as self.filename:
+            self.maze_length = len(self.filename.readline().rstrip())
+            for line in self.filename:
+                if len(line.rstrip()) != self.maze_length:
+                    self.VALID_MAZE = False
+                    raise Exception("Your maze must be a square or a rectangle")
+                else :
+                    self.VALID_MAZE = True
+def main():
+    map01 = Labyrinth("maps/map1.txt")
+    map01.is_maze_valid
+    print(map01.VALID_MAZE)
+main()
+
+
+
+
+
+
+
+"""
 1 utilise settings.txt pour définir les bases du labyrinthe
     boucle for pour lire chaque caractère de settings.txt et lui attribuer une
     caractèristique selon la valeur définie dans constants.py
@@ -11,3 +38,6 @@ class Labyrinth:
         case ou les items apparaitrons aléatoirement)
 2 character se déplace sur labyrinth
 3 définir des coordonnées ?
+4 readline.rstripe pour vérifier nb caractère/ligne si ligne diff stop prog, et pour voir si char et Ennemy
+sont present une seule fois
+"""
